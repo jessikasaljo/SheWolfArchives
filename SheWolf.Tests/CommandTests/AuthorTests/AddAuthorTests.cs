bd2275b2bc3fcec2789c2ee66c3a55563c9ce2d7
jessikasaljo <125219232+jessikasaljo@users.noreply.github.com>
2024-11-23 20:18:25 +0100

    Started adding tests for AddAuthor

---
+++ SheWolf.Tests/CommandTests/AuthorTests/AddAuthorTests.cs
@@ -0,0 +1,61 @@
+using SheWolf.Application.Commands.Authors.AddAuthor;
+using SheWolf.Domain.Entities;
+using SheWolf.Infrastructure.Database;
+
+namespace SheWolf.Tests.CommandTests.AuthorTests
+{
+    public class AddAuthorTests
+    {
+        [Fact]
+        public async Task Handle_ShouldAddAuthorToDatabase()
+        {
+            var mockDatabase = new MockDatabase();
+            var handler = new AddAuthorCommandHandler(mockDatabase);
+
+            var newAuthor = new Author
+            {
+                Name = "Virginia Woolf"
+            };
+
+            var command = new AddAuthorCommand(newAuthor);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Equal(newAuthor.Name, result.Name);
+            Assert.NotEqual(Guid.Empty, result.Id);
+            Assert.Contains(result, mockDatabase.authors);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldCreateAuthorWithEmptyBooksList()
+        {
+            var mockDatabase = new MockDatabase();
+            var handler = new AddAuthorCommandHandler(mockDatabase);
+
+            var newAuthor = new Author
+            {
+                Name = "Sylvia Plath"
+            };
+
+            var command = new AddAuthorCommand(newAuthor);
+
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            Assert.NotNull(result);
+            Assert.Empty(result.Books);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldThrowException_WhenNewAuthorIsNull()
+        {
+            var mockDatabase = new MockDatabase();
+            var handler = new AddAuthorCommandHandler(mockDatabase);
+
+            AddAuthorCommand command = new(null);
+
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => handler.Handle(command, CancellationToken.None));
+            Assert.Equal("NewAuthor", exception.ParamName);
+        }
+    }
+}
