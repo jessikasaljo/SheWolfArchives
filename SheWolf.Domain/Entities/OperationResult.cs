11371f544a5924cfb78121d09a24afadc16aeb3a
jessikasaljo <125219232+jessikasaljo@users.noreply.github.com>
2024-12-05 16:26:33 +0100

    Fixed more formatting

--- SheWolf.Domain/Entities/OperationResult.cs
+++ SheWolf.Domain/Entities/OperationResult.cs
@@ -6,7 +6,7 @@
         public T Data { get; private set; }
         public string Message { get; private set; }
         public string ErrorMessage { get; private set; }
-        
+
         private OperationResult(bool success, T data, string message, string errorMessage)
         {
             Success = success;
@@ -14,12 +14,12 @@
             Message = message;
             ErrorMessage = errorMessage;
         }
-        
-        public static OperationResult<T> Successful(T data, string message = "Operation successful") 
+
+        public static OperationResult<T> Successful(T data, string message = "Operation successful")
         {
             return new OperationResult<T>(true, data, message, null);
         }
-        
+
         public static OperationResult<T> Failure(string errorMessage, string message = "Operation failed")
         {
             return new OperationResult<T>(false, default, message, errorMessage);
